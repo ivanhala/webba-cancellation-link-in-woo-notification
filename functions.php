b02f873b457cb2310fa34a008a692e25331bb2ce
Ivan <ivan@plugion.org>
2020-11-26 12:38:03 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 add_action('woocommerce_order_item_meta_end', 'email_confirmation_display_order_items', 10, 4);
 function email_confirmation_display_order_items($item_id, $item, $order, $plain_text) {
-	if( ! (is_admin() || is_wc_endpoint_url() ) ) {
+	if( ! ( is_admin() ) ) {
 		if( $item->get_product_id() == get_option( 'wbk_woo_product_id', '' ) ){
 			$cancel_link_text = get_option( 'wbk_email_landing_text_cancel', '' );
 			$cancel_link_url  = get_option( 'wbk_email_landing', '' );
